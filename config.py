c85557808aaa7c4bca48f7e6a4b6ad0e7746609f
Ignacio Ortega Aviles <118409806+IgnOrtega@users.noreply.github.com>
2026-07-17 22:32:54 -0400

    Busqueda de sólo ofertas de trabajo, no de prácticas

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from datetime import date
 
-#BASE_URL = "https://firstjob.me/ofertas?semantic=a&type%5B%5D=1"
-BASE_URL = "https://firstjob.me/ofertas?semantic=a"
+BASE_URL = "https://firstjob.me/ofertas?semantic=a&type%5B%5D=1"
+#BASE_URL = "https://firstjob.me/ofertas?semantic=a"
 FECHA_HOY = date.today().isoformat()
